ff63be64ad18827c384bf44f497bb0088c2d775d
Ming Man Chan <chanmmn@hotmail.com>
2020-09-26 19:58:25 +0800

    Commit BlazorAppTemplate
    Commit BlazorAppTemplate

---
+++ BlazorAppTemplate/BlazorAppTemplate/Data/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAppTemplate.Data
+{
+    public class Person
+    {
+        public string Salutation { get; set; }
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
+
+    }
+}
