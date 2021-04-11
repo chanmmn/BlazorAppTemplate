d45d2d9a947ed79e82fd3fe1d9fc8065a013208e
chanmmn <33271651+chanmmn@users.noreply.github.com>
2021-04-10 23:19:11 -0700

    Update Template Component
    Update Template Component

--- TemplateComponent/TemplateComponent/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ TemplateComponent/TemplateComponent/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,70 +13,70 @@ namespace TemplateComponent.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\test\blazor\TemplateComponent\TemplateComponent\_Imports.razor"
+#line 1 "C:\Users\Administrator\Documents\GitHub\BlazorAppTemplate\TemplateComponent\TemplateComponent\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\test\blazor\TemplateComponent\TemplateComponent\_Imports.razor"
+#line 2 "C:\Users\Administrator\Documents\GitHub\BlazorAppTemplate\TemplateComponent\TemplateComponent\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\test\blazor\TemplateComponent\TemplateComponent\_Imports.razor"
+#line 3 "C:\Users\Administrator\Documents\GitHub\BlazorAppTemplate\TemplateComponent\TemplateComponent\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\test\blazor\TemplateComponent\TemplateComponent\_Imports.razor"
+#line 4 "C:\Users\Administrator\Documents\GitHub\BlazorAppTemplate\TemplateComponent\TemplateComponent\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\test\blazor\TemplateComponent\TemplateComponent\_Imports.razor"
+#line 5 "C:\Users\Administrator\Documents\GitHub\BlazorAppTemplate\TemplateComponent\TemplateComponent\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\test\blazor\TemplateComponent\TemplateComponent\_Imports.razor"
+#line 6 "C:\Users\Administrator\Documents\GitHub\BlazorAppTemplate\TemplateComponent\TemplateComponent\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\test\blazor\TemplateComponent\TemplateComponent\_Imports.razor"
+#line 7 "C:\Users\Administrator\Documents\GitHub\BlazorAppTemplate\TemplateComponent\TemplateComponent\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\test\blazor\TemplateComponent\TemplateComponent\_Imports.razor"
+#line 8 "C:\Users\Administrator\Documents\GitHub\BlazorAppTemplate\TemplateComponent\TemplateComponent\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\test\blazor\TemplateComponent\TemplateComponent\_Imports.razor"
+#line 9 "C:\Users\Administrator\Documents\GitHub\BlazorAppTemplate\TemplateComponent\TemplateComponent\_Imports.razor"
 using TemplateComponent;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\test\blazor\TemplateComponent\TemplateComponent\_Imports.razor"
+#line 10 "C:\Users\Administrator\Documents\GitHub\BlazorAppTemplate\TemplateComponent\TemplateComponent\_Imports.razor"
 using TemplateComponent.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using TemplateComponent.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 28 "C:\test\blazor\TemplateComponent\TemplateComponent\Shared\NavMenu.razor"
+#line 33 "C:\Users\Administrator\Documents\GitHub\BlazorAppTemplate\TemplateComponent\TemplateComponent\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
